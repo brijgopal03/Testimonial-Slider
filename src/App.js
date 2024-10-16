09380de595b08237d22118876ef94f8970e07298
brijgopal03 <brijgopaldalmia2003@gmail.com>
2024-10-16 05:44:11 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Testimonials from "./Components/Testimonials";
+import reviews from "./data";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+  <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh] bg-gray-200">
+    <div className="text-center">
+      <h1 className="text-bold text-4xl">Our Testimonials</h1>
+   
+    <div className="h-[4px] w-[1/5] mt-1 bg-violet-400"></div>
+    <Testimonials reviews = {reviews}/>
+    </div>
+  </div>
+  )
 };
 
 export default App;
